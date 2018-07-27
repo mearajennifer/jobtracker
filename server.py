7f2d26ebbfca153a9a83870d1bf03e3a4fe80286
Jennifer Meara <jenniferlmeara@gmail.com>
2018-07-27 00:45:47 +0000

    Initialize HTML templates, add diagrams, create basic active jobs route and template.

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 from jinja2 import StrictUndefined
 from flask import (Flask, render_template, redirect, request, flash, session)
 from flask_debugtoolbar import DebugToolbarExtension
+from sqlalchemy import desc
 from model import (User, Contact, ContactEvent, ContactCode, Company, Job,
                    JobEvent, JobCode, ToDo, ToDoCode, Salary, connect_to_db, db)
 from pprint import pprint
@@ -31,7 +32,7 @@ def show_registration_form():
 
 @app.route('/register', methods=['POST'])
 def register_user():
-    """Gets user input from registration form and checks against databse.
+    """Gets user input from registration form and checks against database.
     Then prompts user to login."""
 
     # get required user data from form
@@ -114,18 +115,55 @@ def logout():
 
 
 @app.route('/dashboard')
-def show_main_jobs_dashboard( ):
-    pass
+def show_dashboard( ):
+    """Shows default dashboard with jobs tracking"""
+
+    return render_template('dashboard.html')
 
 
+@app.route('/dashboard/jobs')
+def show_active_jobs():
+    """Shows list jobs the user is interested in, applied to, or interviewing for."""
+
+    # get user_id from session
+    user_id = session['user_id']
+
+    # query for user job events, return list -- Look at created a db.relationship from users to jobs
+    user_job_events = JobEvent.query.options(db.joinedload('jobs')).filter(JobEvent.user_id == user_id).order_by(desc('date_created')).all()
+
+    # make a set of all job_ids and remove any that are inactive
+    user_job_ids = set(job.job_id for job in user_job_events if job.jobs.active_status == True)
+
+    # grab only the most recent events for each job_id
+    all_active_status = []
+    for user_job_id in user_job_ids:
+        # get all events for one job id
+        events = [event for event in user_job_events if event.job_id == user_job_id]
+        # find that latest event and add to list
+        status = events[0]
+        all_active_status.append(status)
+
+    # get active company objects
+    companies = Company.query.filter(Company.company_id.in_(user_job_ids))
+
+    return render_template('jobs-active.html',
+                           all_active_status=all_active_status,
+                           companies=companies)
 
 
+@app.route('/dashboard/archive-job', methods=['POST'])
+def archive_a_job():
+    """Move job status from active to archive"""
 
+    # get job_id from POST
+    job_id = request.form['job_id']
+    print(job_id)
 
+    # find job in database
+    job = Job.query.filter(Job.job_id == job_id).one()
+    job.active_status = False
 
-
-
-
+    return redirect('/dashboard/jobs')
 
 
 if __name__ == '__main__':
