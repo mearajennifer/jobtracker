715b1306e89d0be7e78eea1826689241376f1d2a
Jennifer Meara <jenniferlmeara@gmail.com>
2018-07-27 23:12:18 +0000

    Define skeleton routes needed for MVP; Create companies list route and HTML page

--- server.py
+++ server.py
@@ -115,99 +115,221 @@ def logout():
 
 
 @app.route('/dashboard')
-def show_dashboard( ):
+def show_dashboard():
     """Shows default dashboard with jobs tracking"""
 
-    return render_template('dashboard.html')
+    if not session:
+        return redirect('/')
+    else:
+        return render_template('dashboard.html')
 
 
 @app.route('/dashboard/jobs')
 def show_active_jobs():
     """Shows list jobs the user is interested in, applied to, or interviewing for."""
 
-    # get user_id from session
-    user_id = session['user_id']
+    # redirect if user is not logged in
+    if not session:
+        return redirect('/')
+    else:
+        # get user_id from session
+        user_id = session['user_id']
 
-    # query for user job events, return list -- Look at created a db.relationship from users to jobs
-    user_job_events = JobEvent.query.options(db.joinedload('jobs')).filter(JobEvent.user_id == user_id).order_by(desc('date_created')).all()
+        # query for user job events, return list -- Look at created a db.relationship from users to jobs
+        user_job_events = JobEvent.query.options(db.joinedload('jobs')).filter(JobEvent.user_id == user_id).order_by(desc('date_created')).all()
 
-    # make a set of all job_ids and remove any that are inactive
-    user_job_ids = set(job.job_id for job in user_job_events if job.jobs.active_status == True)
+        # make a set of all job_ids and remove any that are inactive
+        user_job_ids = set(job.job_id for job in user_job_events if job.jobs.active_status == True)
 
-    # grab only the most recent events for each job_id
-    all_active_status = []
-    for user_job_id in user_job_ids:
-        # get all events for one job id
-        events = [event for event in user_job_events if event.job_id == user_job_id]
-        # find that latest event and add to list
-        status = events[0]
-        all_active_status.append(status)
+        # grab only the most recent events for each job_id
+        all_active_status = []
+        for user_job_id in user_job_ids:
+            # get all events for one job id
+            events = [event for event in user_job_events if event.job_id == user_job_id]
+            # find that latest event and add to list
+            status = events[0]
+            all_active_status.append(status)
 
-    # get active company objects
-    companies = Company.query.filter(Company.company_id.in_(user_job_ids))
+        # get active company objects
+        companies = Company.query.filter(Company.company_id.in_(user_job_ids))
 
-    return render_template('jobs-active.html',
-                           all_active_status=all_active_status,
-                           companies=companies)
+        return render_template('jobs-active.html',
+                               all_active_status=all_active_status,
+                               companies=companies)
 
 
 @app.route('/dashboard/job-status', methods=['POST'])
 def update_job_status():
     """Move job status from active to archive"""
 
-    # get job_id, job_code from POST and user_id from cookie
-    job_id = request.form['job_id']
-    job_code = request.form['job_code']
-    user_id = session['user_id']
+    # redirect if user is not logged in
+    if not session:
+        return redirect('/')
+    else:
+        # get job_id, job_code from POST and user_id from cookie
+        job_id = request.form['job_id']
+        job_code = request.form['job_code']
+        user_id = session['user_id']
 
-    # create job event
-    job_event = JobEvent(job_id=job_id, user_id=user_id, job_code=job_code, date_created=datetime.now())
-    db.session.add(job_event)
+        # create job event
+        job_event = JobEvent(job_id=job_id, user_id=user_id, job_code=job_code, date_created=datetime.now())
+        db.session.add(job_event)
 
-    # find job in database and archive if necessary
-    job = Job.query.filter(Job.job_id == job_id).one()
-    if int(job_code) > 5:
-        job.active_status = False
+        # find job in database and archive if necessary
+        job = Job.query.filter(Job.job_id == job_id).one()
+        if int(job_code) > 5:
+            job.active_status = False
 
-    db.session.commit()
+        db.session.commit()
 
-    return redirect('/dashboard/jobs')
+        return redirect('/dashboard/jobs')
 
 
 @app.route('/dashboard/jobs/archived')
 def show_archived_jobs():
     """ Shows a list of archived jobs the user is no longer tracking."""
 
-    # get user_id from session
-    user_id = session['user_id']
+    # redirect if user is not logged in
+    if not session:
+        return redirect('/')
+    else:
+        # get user_id from session
+        user_id = session['user_id']
 
-    # query for user job events, return list -- Look at created a db.relationship from users to jobs
-    user_job_events = JobEvent.query.options(db.joinedload('jobs')).filter(JobEvent.user_id == user_id).order_by(desc('date_created')).all()
+        # query for user job events, return list -- Look at created a db.relationship from users to jobs
+        user_job_events = JobEvent.query.options(db.joinedload('jobs')).filter(JobEvent.user_id == user_id).order_by(desc('date_created')).all()
 
-    # make a set of all job_ids and remove any that are inactive
-    user_job_ids = set(job.job_id for job in user_job_events if job.jobs.active_status == False)
+        # make a set of all job_ids and remove any that are inactive
+        user_job_ids = set(job.job_id for job in user_job_events if job.jobs.active_status == False)
 
-    # grab only the most recent events for each job_id
-    all_archived = []
-    for user_job_id in user_job_ids:
-        # get all events for one job id
-        events = [event for event in user_job_events if event.job_id == user_job_id]
-        # find that latest event and add to list
-        status = events[0]
-        all_archived.append(status)
+        # grab only the most recent events for each job_id
+        all_archived = []
+        for user_job_id in user_job_ids:
+            # get all events for one job id
+            events = [event for event in user_job_events if event.job_id == user_job_id]
+            # find that latest event and add to list
+            status = events[0]
+            all_archived.append(status)
 
-    # get active company objects
-    companies = Company.query.filter(Company.company_id.in_(user_job_ids))
+        # get active company objects
+        companies = Company.query.filter(Company.company_id.in_(user_job_ids))
 
-    return render_template('jobs-archive.html',
-                           all_archived=all_archived,
-                           companies=companies)
+        return render_template('jobs-archive.html',
+                               all_archived=all_archived,
+                               companies=companies)
 
+
+@app.route('/dashboard/jobs/<job_id>')
+def show_a_job(job_id):
+    """Show all companies a user has interest in."""
+
+    # redirect if user is not logged in
+    if not session:
+        return redirect('/')
+    else:
+        pass
 
 @app.route('/dashboard/jobs/add')
 def add_a_job():
+    """Allow user to add a job"""
 
-    pass
+    # redirect if user is not logged in
+    if not session:
+        return redirect('/')
+    else:
+        pass
+
+
+@app.route('/dashboard/companies')
+def show_all_companies():
+    """Show all companies a user has interest in."""
+
+    # redirect if user is not logged in
+    if not session:
+        return redirect('/')
+    else:
+        # get user_id from session
+        user_id = session['user_id']
+
+        # query for user job events, return list -- Look at created a db.relationship from users to jobs
+        user_job_events = JobEvent.query.options(db.joinedload('jobs')).filter(JobEvent.user_id == user_id).all()
+
+        # make a set of all job ids
+        user_job_ids = set(job.job_id for job in user_job_events)
+
+        # make a list of all companies via job ids
+        companies = {}
+        for job_id in user_job_ids:
+            job = Job.query.filter(Job.job_id == job_id).options(db.joinedload('companies')).first()
+            count = Job.query.filter(Job.company_id == job.companies.company_id).count()
+            companies[job.companies] = count
+
+        return render_template('companies.html', companies=companies)
+
+
+@app.route('/dashboard/companies/<company_id>')
+def show_a_company(company_id):
+    """Show a company a user has interest in."""
+
+    # redirect if user is not logged in
+    if not session:
+        return redirect('/')
+    else:
+        pass
+
+
+@app.route('/dashboard/companies/add')
+def add_a_company():
+    """Allow user to add a company"""
+
+    # redirect if user is not logged in
+    if not session:
+        return redirect('/')
+    else:
+        pass
+
+@app.route('/dashboard/contact')
+def show_all_contacts():
+    """Show all contacts a user is connected to."""
+
+    # redirect if user is not logged in
+    if not session:
+        return redirect('/')
+    else:
+        pass
+
+
+@app.route('/dashboard/contact/<contact_id>')
+def show_a_contact(contact_id):
+    """Show one contacts a user is connected to and all interactions."""
+
+    # redirect if user is not logged in
+    if not session:
+        return redirect('/')
+    else:
+        pass
+
+
+@app.route('/dashboard/contact/add')
+def add_a_contact():
+    """Allow user to add a contact"""
+
+    # redirect if user is not logged in
+    if not session:
+        return redirect('/')
+    else:
+        pass
+
+
+@app.route('/dashboard/user')
+def show_user_profile():
+    """Show user's profile and allow update to information."""
+
+# redirect if user is not logged in
+    if not session:
+        return redirect('/')
+    else:
+        pass
 
 
 
